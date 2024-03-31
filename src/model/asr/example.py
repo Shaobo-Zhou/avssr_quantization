4622377aabf3cac994a79e43967a5fbc940a0423
Blinorot <pmgrin.work@gmail.com>
2024-03-31 12:45:34 +0200

    Add another video encoder, make ASR example smaller

--- src/model/asr/example.py
+++ src/model/asr/example.py
@@ -3,25 +3,35 @@ from torch import nn
 
 class ASRExample(nn.Module):
     def __init__(
-        self, n_input_feats, n_audio_feats, fc_hidden, n_tokens, **kwargs
+        self, n_input_feats, n_hidden_channels, res_reduce, n_tokens, **kwargs
     ) -> None:
         super().__init__()
-        self.asr_net = nn.Sequential(
-            nn.Linear(n_input_feats, fc_hidden),
+        self.conv_part = nn.Sequential(
+            nn.Conv2d(1, n_hidden_channels, 4, 2, 1),
             nn.ReLU(),
-            nn.Linear(
-                fc_hidden, n_audio_feats * n_tokens // 128
-            ),  # decrease resolution
+            nn.Conv2d(n_hidden_channels, n_hidden_channels, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(n_hidden_channels, n_hidden_channels, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(n_hidden_channels, n_hidden_channels, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(n_hidden_channels, 1, 4, 2, 1),
+            nn.ReLU(),
         )
+        self.fc_part = nn.Linear(n_input_feats, n_tokens)
 
         self.n_tokens = n_tokens
+        self.res_reduce = res_reduce
 
     def forward(self, fused_feats, s_audio_length):
-        tokens_logits = self.asr_net(fused_feats)
-        tokens_logits = tokens_logits.view(tokens_logits.shape[0], -1, self.n_tokens)
+        conv_output = self.conv_part(fused_feats)
+
+        conv_output = conv_output.squeeze(1).transpose(1, 2)
+        tokens_logits = self.fc_part(conv_output)
+        # tokens_logits = tokens_logits.view(tokens_logits.shape[0], -1, self.n_tokens)
 
         s_audio_length = (
-            s_audio_length // 128
-        )  # we changed the resolution to x128 lower
+            s_audio_length // self.res_reduce
+        )  # we changed the resolution to x self.res_reduce lower
 
         return tokens_logits, s_audio_length
